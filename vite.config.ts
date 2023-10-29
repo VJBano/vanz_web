b84840ca1dac7e221a9c09ff385c996d2c8fe25c
vincent <vincentjaybano45@gmail.com>
2023-10-29 15:42:35 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-    compression(),
     compression({ algorithm: 'brotliCompress', exclude: [/\.(br)$/, /\.(gz)$/], deleteOriginalAssets: true }),
     compression({
       algorithm: 'gzip', exclude: [/\.(br)$ /, /\.(gz)$/]
